ff7943610d489d1d3667fbca0429ef5b1b75171f
Islam Hassan Abu Aish <islamhaa@yahoo.com>
2019-10-03 09:08:23 +0300

    islam laptop 2019-10=03

---
+++ App.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { StyleSheet,Image, Text, View, TouchableHighlight, ScrollView, TouchableOpacity, PermissionsAndroid } from 'react-native';
+import  LocationA  from './components/location';
+
+export default function App() {
+
+ 
+
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <View style={styles.welcomeContainer}>
+         
+        <TouchableOpacity    onPress={ () => {
+             alert('You tapped the button!');
+         }}>
+          <Image    source={ require('../AwesomeProject/assets/help.gif')}  style={styles.welcomeImage}/>
+        </TouchableOpacity>
+
+        <LocationA/>
+ 
+
+        </View>
+        
+ 
+
+  
+       
+      </ScrollView>
+ 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 256,
+      height: 256,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+ 
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
---
+++ components/location.js
@@ -0,0 +1,70 @@
+
+import React, { Component } from "react";
+import { AppRegistry,  StyleSheet, Dimensions, View } from "react-native";
+import { Container,Text  } from "native-base";
+import { TabNavigator } from "react-navigation";
+
+
+class LocationA extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error:null,
+    }; 
+  }
+
+  testit(){
+    alert('You tapped the button!');
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+       (position) => {
+         console.log("wokeeey");
+         console.log(position);
+         this.setState({
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude,
+           error: null,
+         });
+       },
+       (error) => this.setState({ error: error.message }),
+       { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+     );
+   }
+ 
+  render() {
+    return (
+      <View>
+     <Text> {this.state.latitude} </Text>
+        <Text> {this.state.longitude} </Text>
+        <Text> {this.state.error} </Text> 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+export default LocationA;
+ 
